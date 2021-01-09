37c4a3af8547ce3e2f93a6257101a22a0a0aa595
Jonathan Slauter <jonathanslauter@gmail.com>
2021-01-08 16:57:16 -0800

    final commit

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const p1Button = document.querySelector('#p1Button')
 const p2Button = document.querySelector('#p2Button')
-const reset = document.querySelector('reset')
+const resetButton = document.querySelector('#reset')
 const p1Display = document.querySelector('#p1Display')
 const p2Display = document.querySelector('#p2Display')
+const winningScoreSelect = document.querySelector('#playto')
 
 let p1Score = 0
 let p2Score = 0
-let winningScore = 5
+let winningScore = 3
 let isGameOver = false
 
 p1Button.addEventListener('click', function(){
@@ -14,6 +15,8 @@ p1Button.addEventListener('click', function(){
     p1Score+=1
     if(p1Score === winningScore){
       isGameOver = true
+      p1Button.disabled = true
+      p2Button.disabled = true
     }
         p1Display.textContent = p1Score
       }
@@ -22,10 +25,28 @@ p1Button.addEventListener('click', function(){
 
 p2Button.addEventListener('click', function(){
   if(!isGameOver){
-    p12core+=1
+    p2Score+=1
     if(p2Score === winningScore){
       isGameOver = true
     }
-        p2Display.textContent = p2Score
+        p2Display.innerText = p2Score
       }
 })
+
+winningScoreSelect.addEventListener('change', function(){
+  winningScore = parseInt(this.value)
+  reset()
+})
+
+resetButton.addEventListener('click', reset)
+
+function reset(){
+  isGameOver = false
+  p1Score = 0
+  p2Score = 0
+  p1Display.innerText = 0
+  p2Display.innerText = 0
+  p1Button.disabled = false
+  p2Button.disabled = false
+
+}
